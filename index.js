41c4077e9fe27504916bca25f19f86b020dc3cf3
Martin <martingarcia804@yahoo.com>
2023-01-21 23:27:31 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(result){
+  if (result <= 400) {
+    return "This one is on me!"
+  } else if (result > 400 && result <= 2000) {
+    return "That will be twenty bucks."
+  } else if (result > 2000 && result < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (result > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC"?"Ok, sounds good.":"No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
